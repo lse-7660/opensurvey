b3e4020fb4dd0d2be277d26fb3c687f8836b172a
lse-7660 <lse76609251@gmail.com>
2024-11-01 17:51:55 +0900

    animation/intro, portfolio

--- resources/js/pattern/gsap.js
+++ resources/js/pattern/gsap.js
@@ -2,9 +2,16 @@
 
 // intro-animation
 var tl = gsap.timeline();
-tl.fromTo(".txt-stroke", { y: "-13vw" }, { y: "8vw", duration: 0.6, ease: "expo.out" }, 0.5);
-tl.to(".txt-stroke", { y: "-13vw", duration: 0.38 }, ">");
-tl.to(".txt-fill", { y: "-20vw", duration: 0.6 }, "<+0.1");
+tl.fromTo(".txt-stroke", { y: "-13vw" }, { y: "8vw", duration: 0.6, ease: "expo.out" }, 0.1);
+tl.to(".txt-stroke", { y: "-13vw", duration: 0.2 }, ">");
+tl.to(".txt-fill", { y: "-22vw", duration: 0.4 }, "<+0.1");
 tl.to(".txt-fill", { y: "-13vw", duration: 0.2 }, ">+0.2");
 tl.to(".txt-fill", { letterSpacing: "30px", duration: 1, ease: "expo.out" }, ">+0.3");
-tl.to(".txt-fill", { letterSpacing: "0", scale: 1, duration: 0.1 }, ">+0.2");
+tl.to(".txt-fill2", { letterSpacing: "30px", duration: 1.6, ease: "expo.out" }, "<");
+tl.to(".bg-fill", { "--clip": "100vh", duration: 1.6, ease: "expo.out" }, "<");
+tl.to(".intro-animation", { backgroundColor: "#ffffff", duration: 0.1 }, "<+0.1");
+tl.to(".txt-fill2", { letterSpacing: "0", color: "black", duration: 0.1 });
+tl.to(".intro-animation", { display: "none" });
+tl.to(".intro__content", { display: "block" }, "<+0.5");
+tl.to(".inner-intro", { gap: "10vh", duration: 1.2 }, "<");
+tl.to(".intro", { height: "60vh", duration: 1, ease: "expo.out" }, ">+0.5");
